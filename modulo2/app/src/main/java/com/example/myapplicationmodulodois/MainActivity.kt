66fcddb38078da64548b57973aaf51528e204cdb
ElielSilva <silvaeliel940@gmail.com>
2025-10-01 20:51:25 -0300

    Adiciona projeto do módulo 2 com MVVM e Compose

---
+++ modulo2/app/src/main/java/com/example/myapplicationmodulodois/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplicationmodulodois
+
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+// MainActivity.kt
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.myapplicationmodulodois.ui.screen.tasks.TasksScreen
+import com.example.myapplicationmodulodois.ui.theme.MyApplicationModuloDoisTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationModuloDoisTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TasksScreen()
+                }
+            }
+        }
+    }
+}
---
+++ modulo2/app/src/main/java/com/example/myapplicationmodulodois/ui/screen/tasks/UiState.kt
@@ -0,0 +1,8 @@
+package com.example.myapplicationmodulodois.ui.screen.tasks
+
+// UiState.kt
+sealed class UiState<out T> {
+    object Loading : UiState<Nothing>()
+    data class Success<T>(val data: T) : UiState<T>()
+    data class Error(val message: String) : UiState<Nothing>()
+}
